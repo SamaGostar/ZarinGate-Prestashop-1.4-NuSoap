cdc8dc1d567e93a1e15ba624beb25b120231466c
Masoud A <amini141@gmail.com>
2013-07-09 11:19:54 +0430

    update

--- module/zarinpalzaringatepayment/payment.php
+++ module/zarinpalzaringatepayment/payment.php
@@ -2,13 +2,13 @@
 
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../header.php');
-include_once(dirname(__FILE__).'/zarinpalpayment.php');
+include_once(dirname(__FILE__).'/zarinpalzaringatepayment.php');
 
 if (!$cookie->isLogged())
     Tools::redirect('authentication.php?back=order.php');
 	
-$zarinpalpayment= new zarinpalpayment();
-echo $zarinpalpayment->execPayment($cart);
+$zarinpalzaringatepayment= new zarinpalzaringatepayment();
+echo $zarinpalzaringatepayment->execPayment($cart);
 
 include_once(dirname(__FILE__).'/../../footer.php');
 
