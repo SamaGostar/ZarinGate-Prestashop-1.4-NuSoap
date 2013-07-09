d653dc816e37bb394d399d1ff8094a911c0428a6
Masoud A <amini141@gmail.com>
2013-07-09 11:36:21 +0430

    Update validation.php

--- module/zarinpalzaringatepayment/validation.php
+++ module/zarinpalzaringatepayment/validation.php
@@ -2,23 +2,23 @@
 
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../header.php');
-include_once(dirname(__FILE__).'/zarinpalpayment.php');
+include_once(dirname(__FILE__).'/zarinpalzaringatepayment.php');
 
 	if (!$cookie->isLogged())
 		Tools::redirect('authentication.php?back=order.php');
                 
         $currency_default = Currency::getCurrency(intval(Configuration::get('PS_CURRENCY_DEFAULT')));        
-        $zarinpalpayment= new zarinpalpayment(); // Create an object for order validation and language translations
+        $zarinpalzaringatepayment= new zarinpalzaringatepayment(); // Create an object for order validation and language translations
 		
 		$order_cart = new Cart(intval($_COOKIE["OrderId"]));
 		
 		$PurchaseAmount=number_format(Tools::convertPrice(intval($_COOKIE["PurchaseAmount"]), $currency_default), 0, '', '');
 		$OrderAmount=number_format(Tools::convertPrice($order_cart->getOrderTotal(true, 3), $currency_default), 0, '', '');
 	
-        $result = $zarinpalpayment->confirmPayment($PurchaseAmount);
+        $result = $zarinpalzaringatepayment->confirmPayment($PurchaseAmount);
 	
 	// We now think that the response is valid, so we can look at the result      
-	$result = $zarinpalpayment->showMessages($result);
+	$result = $zarinpalzaringatepayment->showMessages($result);
 
 	// if we have a valid completed order, validate it
 
@@ -28,9 +28,9 @@ include_once(dirname(__FILE__).'/zarinpalpayment.php');
 	if (($result==1) and ($hash==$_COOKIE["ZARINPAL_HASH"]))
 	{
 		if($PurchaseAmount==$OrderAmount)
-			 $zarinpalpayment->validateOrder(intval($_COOKIE["OrderId"]), _PS_OS_PAYMENT_,$order_cart->getOrderTotal(true, 3), $zarinpalpayment->displayName, $zarinpalpayment->l('Payment Accepted'), array(), $cookie->id_currency);
+			 $zarinpalzaringatepayment->validateOrder(intval($_COOKIE["OrderId"]), _PS_OS_PAYMENT_,$order_cart->getOrderTotal(true, 3), $zarinpalzaringatepayment->displayName, $zarinpalzaringatepayment->l('Payment Accepted'), array(), $cookie->id_currency);
 		else
-			 $zarinpalpayment->validateOrder(intval($_COOKIE["OrderId"]), _PS_OS_PAYMENT_,$PurchaseAmount, $zarinpalpayment->displayName, $zarinpalpayment->l('Payment Accepted'), array(), $cookie->id_currency);
+			 $zarinpalzaringatepayment->validateOrder(intval($_COOKIE["OrderId"]), _PS_OS_PAYMENT_,$PurchaseAmount, $zarinpalzaringatepayment->displayName, $zarinpalzaringatepayment->l('Payment Accepted'), array(), $cookie->id_currency);
 
 
         setcookie("ZARINPAL_HASH", "", -1);
